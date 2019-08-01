8c97ad1208e24a4bb20dc3df54c279bef68492ae
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-08-01 13:30:47 +1000

    Hacky fix to <<< >>> syntax error

--- main.cu
+++ main.cu
@@ -4,6 +4,18 @@
 #include "device_launch_parameters.h"
 
 
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
+#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#else
+#define CUDA_CALLABLE_MEMBER
+#define KERNEL_ARGS2(grid, block)
+#define KERNEL_ARGS3(grid, block, sh_mem)
+#define KERNEL_ARGS4(grid, block, sh_mem, stream)
+#endif
+
 #include <stdio.h>
 
 const int N = 16;
@@ -33,7 +45,7 @@ int main()
 
 	dim3 dimBlock(blocksize, 1);
 	dim3 dimGrid(1, 1);
-	hello << <dimGrid, dimBlock >> > (ad, bd);
+	hello KERNEL_ARGS2(dimGrid, dimBlock) (ad, bd);
 	cudaMemcpy(a, ad, csize, cudaMemcpyDeviceToHost);
 	cudaFree(ad);
 	cudaFree(bd);
